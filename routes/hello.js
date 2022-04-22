5afa89ba7394ae55978a1fc501d5a934a4ca8de9
Lissana <liss.glace471@gmail.com>
2022-04-22 14:32:19 +0900

    feat: RSSの取得と取得データの表示

--- routes/hello.js
+++ routes/hello.js
@@ -1,27 +1,34 @@
 const express = require('express');
 const router = express.Router();
 
+const http = require('https');
+const parseString = require('xml2js').parseString;
+
+
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    let msg = `※なにか書いて送信してください。`;
-    if(req.session.message != undefined){
-        msg = `Last Message: ${req.session.message}`;
-    }
-    const data = {
-        title: 'Hello',
-        content: msg,
-    }
-    res.render('hello', data);
+    const opt = {
+        host: 'news.google.com',
+        port: 443,
+        path: '/rss?ie=UTF-8&oe=UTF-8&hl=en-US&gl=US&ceid=US:en',
+    };
+    http.get(opt, (res2) => {
+        let body = '';
+        res2.on('data', (data) => {
+            body += data;
+        });
+        res2.on('end', () => {
+            parseString(body.trim(), (err, result) => {
+                const data = {
+                    title: 'Hello',
+                    content: result.rss.channel[0].item,
+                };
+                res.render('hello', data);
+            });
+        });
+    });
 });
 
-router.post('/post', (req, res, next) => {
-    const msg = req.body['message'];
-    req.session.message = msg;
-    const data = {
-        title: 'Hello',
-        content: `Last Message: ${req.session.message}`,
-    };
-    res.render('hello', data);
-});
 
 module.exports = router;
