206c9e9cb62e2937b2e351f33da521a80381a2e6
Lissana <liss.glace471@gmail.com>
2022-04-22 16:05:06 +0900

    feat: データベースの読み込み及び表示

--- routes/hello.js
+++ routes/hello.js
@@ -1,31 +1,22 @@
 const express = require('express');
-const router = express.Router();
-
 const http = require('https');
-const parseString = require('xml2js').parseString;
+const sqlite3 = require('sqlite3');
 
-
+const router = express.Router();
+const db = new sqlite3.Database('mydb.sqlite3');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    const opt = {
-        host: 'news.google.com',
-        port: 443,
-        path: '/rss?ie=UTF-8&oe=UTF-8&hl=en-US&gl=US&ceid=US:en',
-    };
-    http.get(opt, (res2) => {
-        let body = '';
-        res2.on('data', (data) => {
-            body += data;
-        });
-        res2.on('end', () => {
-            parseString(body.trim(), (err, result) => {
+    db.serialize(() => {
+        db.all('select * from mydata', (err, rows) => {
+            if(!err){
                 const data = {
                     title: 'Hello',
-                    content: result.rss.channel[0].item,
+                    content: rows,
                 };
                 res.render('hello', data);
-            });
+            }
+            console.error(err);
         });
     });
 });
