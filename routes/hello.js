28164da416b5046e094ab29487d7d2e252523371
Lissana <liss.glace471@gmail.com>
2022-04-23 11:21:10 +0900

    feat: データベースの更新処理
    5-4. データベースの基本をマスターする

--- routes/hello.js
+++ routes/hello.js
@@ -8,7 +8,7 @@ const db = new sqlite3.Database('mydb.sqlite3');
 router.get('/', function (req, res, next) {
     db.serialize(() => {
         db.all('select * from mydata', (err, rows) => {
-            if(!err){
+            if (!err) {
                 const data = {
                     title: 'Hello',
                     content: rows,
@@ -41,7 +41,7 @@ router.get('/show', (req, res, next) => {
     db.serialize(() => {
         const q = 'select * from mydata where id = ?';
         db.get(q, [id], (err, row) => {
-            if(!err){
+            if (!err) {
                 const data = {
                     title: 'hello/show',
                     content: `id = ${id}のレコード`,
@@ -51,6 +51,34 @@ router.get('/show', (req, res, next) => {
             }
         });
     });
+});
+
+router.get('/edit', (req, res, next) => {
+    console.log(`クエリパラメータ:${req.query.id}`);
+    const id = req.query.id ?? 1;
+    db.serialize(() => {
+        const q = 'select * from mydata where id = ?';
+        db.get(q, [id], (err, row) => {
+            if (!err) {
+                const data = {
+                    title: 'hello/edit',
+                    content: `id = ${id}のレコードを編集`,
+                    mydata: row,
+                };
+                res.render('hello/edit', data);
+            }
+        });
+    });
+});
+
+router.post('/edit', (req, res, next) => {
+    const id = req.body.id;
+    const nm = req.body.name;
+    const ml = req.body.mail;
+    const ag = req.body.age;
+    const q = 'update mydata set name = ?, mail = ?, age = ? where id = ?';
+    db.run(q, nm, ml, ag, id);
+    res.redirect('/hello');
 });
 
 module.exports = router;
