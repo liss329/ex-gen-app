f4d91b49c90239c56beaa941ceb3372142dbdeff
Lissana <liss.glace471@gmail.com>
2022-04-23 10:32:07 +0900

    feat: データベースの更新処理

--- routes/hello.js
+++ routes/hello.js
@@ -5,7 +5,6 @@ const sqlite3 = require('sqlite3');
 const router = express.Router();
 const db = new sqlite3.Database('mydb.sqlite3');
 
-/* GET home page. */
 router.get('/', function (req, res, next) {
     db.serialize(() => {
         db.all('select * from mydata', (err, rows) => {
@@ -14,12 +13,27 @@ router.get('/', function (req, res, next) {
                     title: 'Hello',
                     content: rows,
                 };
-                res.render('hello', data);
+                res.render('hello/index', data);
             }
             console.error(err);
         });
     });
 });
 
+router.get('/add', (req, res, next) => {
+    const data = {
+        title: 'Hello/Add',
+        content: '新しいレコードを入力：',
+    };
+    res.render('hello/add', data);
+});
+
+router.post('/add', (req, res, next) => {
+    let nm = req.body.name;
+    let ml = req.body.mail;
+    let ag = req.body.age;
+    db.run('insert into mydata (name, mail, age) values (?, ?, ?)', nm, ml, ag);
+    res.redirect('/hello');
+});
 
 module.exports = router;
